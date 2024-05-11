3ac5da3eceb6f7101784a748f566187a0c95d436
Rakib Hasan <rakibnkhadija@gmail.com>
2024-05-11 16:15:01 +0600

    connect with mongodb and create env file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
@@ -16,6 +17,32 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdbwfxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+        const foodCollection = client.db('coolinaryDB').collection('food')
+
+        app.post('/food', async(req, res)=>{
+            const food = req.body
+            console.log(food)
+        })
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+  }
+  run().catch(console.dir);
 
 
 app.get('/', (req, res)=>{
